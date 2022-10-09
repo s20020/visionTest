8c782522db8c92cbec912e36fc4897a68722d875
s20020 <s20020@std.it-college.ac.jp>
2022-10-09 18:23:27 +0900

    同じフォルダーかつファイル名のものはトーストで弾くことに成功。

--- app/src/main/java/jp/ac/it_college/std/s20020/visiontest/File_Foleder_Input.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/visiontest/File_Foleder_Input.kt
@@ -5,6 +5,8 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Toast
@@ -41,19 +43,43 @@ class File_Foleder_Input : AppCompatActivity() {
         all_english = intent.getStringExtra("ES").toString()
         all_japanese = intent.getStringExtra("JA").toString()
 
-        println(all_english)
-        println(all_japanese)
 
         //DatabaseHelperオブジェクトを生成
         _helper = DatabaseHelper(applicationContext)
+
+        //決定ボタンフォルダ作成ボタンを押せない状態にしておく
+        binding.folderOkBtn.isEnabled = false
+        binding.folderAddBtn.isEnabled  = false
+
+
+        binding.failNameEdit.addTextChangedListener(object : TextWatcher {
+            //テキストが編集される前に発生するイベント
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            //テキストが変更された直後(日本語IMEの変換候補が出ている間も含む)
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            //テキストが変更された直後(入力が確定された後)
+            override fun afterTextChanged(p0: Editable?) {
+
+                if(binding.failNameEdit.text.toString() != ""){
+                    binding.folderOkBtn.isEnabled = true
+                    binding.folderAddBtn.isEnabled = true
+                } else {
+                    binding.folderOkBtn.isEnabled = false
+                    binding.folderAddBtn.isEnabled  = false
+                }
+            }
+
+        })
 
 
         //DBからfolder_nameを取り出す
         val select_folder_name = """
             SELECT distinct folder_name FROM main
         """.trimIndent()
-
-
 
         //mainDB挿入命令
         val insert_main = """
@@ -119,25 +145,43 @@ class File_Foleder_Input : AppCompatActivity() {
 
                 val db = _helper.writableDatabase
 
-                Toast.makeText(this, "$folder_name と入力しました", Toast.LENGTH_SHORT).show()
+                //フォルダー名、ファイル名が全く同じものがないか判定する
+                //判定のために列を数える
+                val select = """
+                SELECT COUNT(*) FROM main
+                WHERE folder_name = '${folder_name}' 
+                AND file_name = '${file_name}'
+                """.trimIndent()
 
-                //SQL文字列をもとにプリペアドステートメントを取得
-                val stmt1 = db.compileStatement(insert_main)
+                val c = db.rawQuery(select, null)
+                c.moveToNext()
+                println("unko")
+                println(c.getInt(0).toString())
 
-                stmt1.bindLong(1, _id.toLong())
-                stmt1.bindString(2, folder_name)
-                stmt1.bindString(3, file_name)
-                stmt1.bindString(4, all_english)
-                stmt1.bindString(5, all_japanese)
 
-                println(_id)
-                println(folder_name)
-                println(file_name)
-                println(all_english)
-                println(all_japanese)
 
-                //データベースへのinsert実行。
-                stmt1.executeInsert()
+                //かぶっているものがなければ実行する
+                if(c.getInt(0).toString() != "0") {
+                    Toast.makeText(this, "${folder_name}に${file_name}が既に存在しています", Toast.LENGTH_SHORT).show()
+                }else {
+                    //SQL文字列をもとにプリペアドステートメントを取得
+                    val stmt1 = db.compileStatement(insert_main)
+
+                    stmt1.bindLong(1, _id.toLong())
+                    stmt1.bindString(2, folder_name)
+                    stmt1.bindString(3, file_name)
+                    stmt1.bindString(4, all_english)
+                    stmt1.bindString(5, all_japanese)
+
+                    println(_id)
+                    println(folder_name)
+                    println(file_name)
+                    println(all_english)
+                    println(all_japanese)
+
+                    //データベースへのinsert実行。
+                    stmt1.executeInsert()
+                }
 
                 //************************************************************
 
@@ -146,6 +190,8 @@ class File_Foleder_Input : AppCompatActivity() {
 
                 //新しいフォルダを作った場合はすぐに決定ボタンを押した時点で完了して
                 //初期画面に遷移する。
+
+
 //                val intent = Intent(this, StudyOrCreate::class.java)
 //                startActivity(intent)
 
@@ -171,3 +217,4 @@ class File_Foleder_Input : AppCompatActivity() {
         super.onDestroy()
     }
 }
+
