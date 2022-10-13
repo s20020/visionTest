17b27e8c07fb170c5173d12d40c00a19fcebc172
s20020 <s20020@std.it-college.ac.jp>
2022-10-13 15:49:32 +0900

    一旦、２０個つなげて出力はできる。 次は終了ボタンと、スピーカー、最初の左ボタンをせなくするような処理を考える。

--- app/src/main/java/jp/ac/it_college/std/s20020/visiontest/StudyEnglish.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/visiontest/StudyEnglish.kt
@@ -11,10 +11,15 @@ class StudyEnglish : AppCompatActivity() {
 
     private lateinit var _helper: DatabaseHelper
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStudyEnglishBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var list_number = intent.getIntExtra("LIST_NUMBER",0)
+
 
         val en_list = intent.getStringArrayListExtra("ENLIST")
         val ja_list = intent.getStringArrayListExtra("JALIST")
@@ -26,11 +31,19 @@ class StudyEnglish : AppCompatActivity() {
         println(en_list?.get(10))
         println(ja_list?.get(10))
 
+        //２つでセットのため÷２している
+        binding.studyEnText.text = en_list?.get(list_number/2)
+
+
+
 
         binding.rightBtn.setOnClickListener{
             val intent = Intent(this, StudyJapanese::class.java)
             intent.putStringArrayListExtra("ENLIST", en_list)
             intent.putStringArrayListExtra("JALIST", ja_list)
+            list_number += 1
+            intent.putExtra("LIST_NUMBER", list_number)
+            println(list_number)
             startActivity(intent)
         }
 
--- app/src/main/java/jp/ac/it_college/std/s20020/visiontest/StudyJapanese.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/visiontest/StudyJapanese.kt
@@ -7,10 +7,16 @@ import jp.ac.it_college.std.s20020.visiontest.databinding.ActivityStudyJapaneseB
 
 class StudyJapanese : AppCompatActivity() {
     private lateinit var binding : ActivityStudyJapaneseBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStudyJapaneseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var list_number = intent.getIntExtra("LIST_NUMBER",0)
+
 
         val en_list = intent.getStringArrayListExtra("ENLIST")
         val ja_list = intent.getStringArrayListExtra("JALIST")
@@ -22,10 +28,15 @@ class StudyJapanese : AppCompatActivity() {
         println(en_list?.get(10))
         println(ja_list?.get(10))
 
+        binding.studyJaText.text = ja_list?.get(list_number/2)
+
         binding.rightBtn2.setOnClickListener{
             val intent = Intent(this, StudyEnglish::class.java)
             intent.putStringArrayListExtra("ENLIST", en_list)
             intent.putStringArrayListExtra("JALIST", ja_list)
+            list_number += 1
+            intent.putExtra("LIST_NUMBER", list_number)
+            println(list_number)
             startActivity(intent)
         }
 
