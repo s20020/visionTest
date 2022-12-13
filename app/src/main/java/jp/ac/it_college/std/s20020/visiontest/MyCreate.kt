af236f14fb3dd43c3d5fd2f5cf2d16d1b7cdb228
s20020 <s20020@std.it-college.ac.jp>
2022-12-13 19:14:50 +0900

    翻訳したものをリスト表示することができた。 次はそのリストをEditTextに入れ込む作業

--- app/src/main/java/jp/ac/it_college/std/s20020/visiontest/MyCreate.kt
+++ app/src/main/java/jp/ac/it_college/std/s20020/visiontest/MyCreate.kt
@@ -17,7 +17,6 @@ import kotlin.properties.Delegates
 class MyCreate : AppCompatActivity() {
     private lateinit var binding: ActivityMyCreateBinding
 
-    val s_list = listOf<String>()
 
     var all_english = ""
     var all_japanese = ""
@@ -42,11 +41,6 @@ class MyCreate : AppCompatActivity() {
 
         ID = intent.getIntExtra("ID",0)
 
-
-        println(ja_list)
-        println(en_list)
-        println(ID)
-
         //写真ORフォルダからとってきたものだったら、PutTextを実行する
         if(ID == 1) {
             PutText()
@@ -62,13 +56,31 @@ class MyCreate : AppCompatActivity() {
 
         binding.transBtn.setOnClickListener{
 
+            val a = binding.edit11.text.toString()
+            println(a)
+            //今かかれているものを取得
+            val list = ListAddEn()
+            println(list)
+            val list2 = arrayListOf<String>()
             lifecycleScope.launch {
                 withContext(Dispatchers.IO) {
                     val translator = Translator(API_KEY)
-                    val result = translator.translateText("hello", null, "ja")
-                    println(result.text)
+                    var i = 0
+                    while(i < 20) {
+                        if(list[i] != ""){
+                            val result = translator.translateText(list[i], null, "ja")
+                            list2.add(result.text)
+                        }else {
+                            val result = ""
+                            list2.add(result)
+                        }
+                        i++
+                    }
+                    println(list2)
+
                 }
             }
+
         }
 
 
@@ -76,57 +88,14 @@ class MyCreate : AppCompatActivity() {
         binding.okBtn.setOnClickListener{
 
             //英単語をカンマ区切りの文字列にして格納
-            val list = arrayListOf<String>()
-            list.add(binding.edit11.text.toString())
-            list.add(binding.edit21.text.toString())
-            list.add(binding.edit31.text.toString())
-            list.add(binding.edit41.text.toString())
-            list.add(binding.edit51.text.toString())
-            list.add(binding.edit61.text.toString())
-            list.add(binding.edit71.text.toString())
-            list.add(binding.edit81.text.toString())
-            list.add(binding.edit91.text.toString())
-            list.add(binding.edit101.text.toString())
-            list.add(binding.edit111.text.toString())
-            list.add(binding.edit121.text.toString())
-            list.add(binding.edit131.text.toString())
-            list.add(binding.edit141.text.toString())
-            list.add(binding.edit151.text.toString())
-            list.add(binding.edit161.text.toString())
-            list.add(binding.edit171.text.toString())
-            list.add(binding.edit181.text.toString())
-            list.add(binding.edit191.text.toString())
-            list.add(binding.edit201.text.toString())
+            val list = ListAddEn()
 
             all_english = list.joinToString(separator = ",")
 
 
             //日本語をカンマ区切りの文字列にして格納
-            val list2 = arrayListOf<String>()
-            list2.add(binding.edit12.text.toString())
-            list2.add(binding.edit22.text.toString())
-            list2.add(binding.edit32.text.toString())
-            list2.add(binding.edit42.text.toString())
-            list2.add(binding.edit52.text.toString())
-            list2.add(binding.edit62.text.toString())
-            list2.add(binding.edit72.text.toString())
-            list2.add(binding.edit82.text.toString())
-            list2.add(binding.edit92.text.toString())
-            list2.add(binding.edit102.text.toString())
-            list2.add(binding.edit112.text.toString())
-            list2.add(binding.edit122.text.toString())
-            list2.add(binding.edit132.text.toString())
-            list2.add(binding.edit142.text.toString())
-            list2.add(binding.edit152.text.toString())
-            list2.add(binding.edit162.text.toString())
-            list2.add(binding.edit172.text.toString())
-            list2.add(binding.edit182.text.toString())
-            list2.add(binding.edit192.text.toString())
-            list2.add(binding.edit202.text.toString())
-
+            val list2 = ListAddEn()
             all_japanese = list2.joinToString(separator = ",")
-            
-
 
             intent = Intent(this, File_Foleder_Input::class.java)
             intent.putExtra("ES", all_english)
@@ -142,26 +111,20 @@ class MyCreate : AppCompatActivity() {
     //MyCreateに撮ってきた値をおく。
     fun PutText() {
 
-        val jlist = intent.getStringArrayListExtra("JA")
+//        val jlist = intent.getStringArrayListExtra("JA")
         val elist = intent.getStringArrayListExtra("EN")
 
-        println(jlist)
-        println(elist)
-        println(ja_list)
-        println(en_list)
-        println(jlist?.size)
-        println(elist?.size)
 
 
-        var a = 0
-        try{
-            while(a < jlist?.size!!){
-                ja_list[a] = jlist[a]
-                a++
-            }
-        }catch (e: Exception){
-            Log.e("getAddres","GetAddressErr")
-        }
+//        var a = 0
+//        try{
+//            while(a < jlist?.size!!){
+//                ja_list[a] = jlist[a]
+//                a++
+//            }
+//        }catch (e: Exception){
+//            Log.e("getAddres","GetAddressErr")
+//        }
 
         try{
             var b = 0
@@ -175,6 +138,104 @@ class MyCreate : AppCompatActivity() {
 
 
         //英語
+        binding.edit11.setText(en_list[0])
+        binding.edit21.setText(en_list[1])
+        binding.edit31.setText(en_list[2])
+        binding.edit41.setText(en_list[3])
+        binding.edit51.setText(en_list[4])
+        binding.edit61.setText(en_list[5])
+        binding.edit71.setText(en_list[6])
+        binding.edit81.setText(en_list[7])
+        binding.edit91.setText(en_list[8])
+        binding.edit101.setText(en_list[9])
+        binding.edit111.setText(en_list[10])
+        binding.edit121.setText(en_list[11])
+        binding.edit131.setText(en_list[12])
+        binding.edit141.setText(en_list[13])
+        binding.edit151.setText(en_list[14])
+        binding.edit161.setText(en_list[15])
+        binding.edit171.setText(en_list[16])
+        binding.edit181.setText(en_list[17])
+        binding.edit191.setText(en_list[18])
+        binding.edit201.setText(en_list[19])
+
+
+
+        //日本語
+//        binding.edit12.setText(ja_list[0])
+//        binding.edit22.setText(ja_list[1])
+//        binding.edit32.setText(ja_list[2])
+//        binding.edit42.setText(ja_list[3])
+//        binding.edit52.setText(ja_list[4])
+//        binding.edit62.setText(ja_list[5])
+//        binding.edit72.setText(ja_list[6])
+//        binding.edit82.setText(ja_list[7])
+//        binding.edit92.setText(ja_list[8])
+//        binding.edit102.setText(ja_list[9])
+//        binding.edit112.setText(ja_list[10])
+//        binding.edit122.setText(ja_list[11])
+//        binding.edit132.setText(ja_list[12])
+//        binding.edit142.setText(ja_list[13])
+//        binding.edit152.setText(ja_list[14])
+//        binding.edit162.setText(ja_list[15])
+//        binding.edit172.setText(ja_list[16])
+//        binding.edit182.setText(ja_list[17])
+//        binding.edit192.setText(ja_list[18])
+//        binding.edit202.setText(ja_list[19])
+    }
+
+    fun ListAddEn() : ArrayList<String>{
+        val list = arrayListOf<String>()
+        list.add(binding.edit11.text.toString())
+        list.add(binding.edit21.text.toString())
+        list.add(binding.edit31.text.toString())
+        list.add(binding.edit41.text.toString())
+        list.add(binding.edit51.text.toString())
+        list.add(binding.edit61.text.toString())
+        list.add(binding.edit71.text.toString())
+        list.add(binding.edit81.text.toString())
+        list.add(binding.edit91.text.toString())
+        list.add(binding.edit101.text.toString())
+        list.add(binding.edit111.text.toString())
+        list.add(binding.edit121.text.toString())
+        list.add(binding.edit131.text.toString())
+        list.add(binding.edit141.text.toString())
+        list.add(binding.edit151.text.toString())
+        list.add(binding.edit161.text.toString())
+        list.add(binding.edit171.text.toString())
+        list.add(binding.edit181.text.toString())
+        list.add(binding.edit191.text.toString())
+        list.add(binding.edit201.text.toString())
+        return list
+    }
+
+    fun ListAddJa() : ArrayList<String> {
+        val list2 = arrayListOf<String>()
+        list2.add(binding.edit12.text.toString())
+        list2.add(binding.edit22.text.toString())
+        list2.add(binding.edit32.text.toString())
+        list2.add(binding.edit42.text.toString())
+        list2.add(binding.edit52.text.toString())
+        list2.add(binding.edit62.text.toString())
+        list2.add(binding.edit72.text.toString())
+        list2.add(binding.edit82.text.toString())
+        list2.add(binding.edit92.text.toString())
+        list2.add(binding.edit102.text.toString())
+        list2.add(binding.edit112.text.toString())
+        list2.add(binding.edit122.text.toString())
+        list2.add(binding.edit132.text.toString())
+        list2.add(binding.edit142.text.toString())
+        list2.add(binding.edit152.text.toString())
+        list2.add(binding.edit162.text.toString())
+        list2.add(binding.edit172.text.toString())
+        list2.add(binding.edit182.text.toString())
+        list2.add(binding.edit192.text.toString())
+        list2.add(binding.edit202.text.toString())
+
+        return list2
+    }
+
+    fun setText() {
         binding.edit11.setText(en_list[0])
         binding.edit21.setText(en_list[1])
         binding.edit31.setText(en_list[2])
